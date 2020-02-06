c39f9cd5285dc50a1cacb08e72d6555498a15946
ravenblack24 <mysweetr0se@hotmail.com>
2020-02-06 15:47:46 +0000

    Update 8kyu_ReversedStrings.js

--- 8kyu_ReversedStrings.js
+++ 8kyu_ReversedStrings.js
@@ -4,6 +4,12 @@ Complete the solution so that it reverses the string value passed into it.
 
 solution('world'); // returns 'dlrow'
 
+Test Cases:
+Test.expect(solution('world') == 'dlrow')
+Test.expect(solution('hello') == 'olleh')
+Test.expect(solution('') == '')
+Test.expect(solution('h') == 'h')
+
 */
 
 function solution(str){
