1ae1b143cf44870a67a464eedf4783cddc7dc7a5
ravenblack24 <mysweetr0se@hotmail.com>
2020-04-03 11:33:11 +0100

    Create 7kyu_RegexBasics-isItAVowel.js

---
+++ 7kyu_RegexBasics-isItAVowel.js
@@ -0,0 +1,19 @@
+/**
+Implement the function which should return true if given object is a vowel (meaning a, e, i, o, u), and false otherwise.
+
+Tests:
+describe('vowel', () => {
+  it('Basic tests', () => {
+    Test.assertEquals(''.vowel(), false);
+    Test.assertEquals('a'.vowel(), true);
+    Test.assertEquals('E'.vowel(), true);
+    Test.assertEquals('ou'.vowel(), false);
+    Test.assertEquals('z'.vowel(), false);
+    Test.assertEquals('lol'.vowel(), false);
+  });
+});
+*/
+String.prototype.vowel = function() {
+  let regex = /^(a|e|i|o|u){1}$/ig;
+  return regex.test(this);
+};
