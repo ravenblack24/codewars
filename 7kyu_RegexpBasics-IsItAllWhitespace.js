4541c694a64afacabd8df3a239dcd82ba029b2eb
ravenblack24 <mysweetr0se@hotmail.com>
2020-04-03 11:51:40 +0100

    Create 7kyu_RegexpBasics-IsItAllWhitespace.js

---
+++ 7kyu_RegexpBasics-IsItAllWhitespace.js
@@ -0,0 +1,22 @@
+/** 
+
+Implement String#whitespace?(str) (Ruby), String.prototype.whitespace(str) (JavaScript), String::whitespace(str) (CoffeeScript), or whitespace(str) (Python), which should return true/True if given object consists exclusively of zero or more whitespace characters, false/False otherwise.
+
+Test.describe("Basic tests",_=>{
+Test.assertEquals("".whitespace(), true);
+Test.assertEquals(" ".whitespace(), true);
+Test.assertEquals("\n\r\n\r".whitespace(), true);
+Test.assertEquals("a".whitespace(), false);
+Test.assertEquals("w\n".whitespace(), false);
+Test.assertEquals("\t".whitespace(), true);
+Test.assertEquals(" a\n".whitespace(), false);
+Test.assertEquals("\t \n\r\n  ".whitespace(), true);
+Test.assertEquals("\n\r\n\r ".whitespace(), true);
+Test.assertEquals("\n\r\n\r 3".whitespace(), false);
+})
+
+*/
+
+String.prototype.whitespace=function(){
+  return /^(\s+){0,1}$/g.test(this);
+}
