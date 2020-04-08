85882245627717e172e865ccdacc89c76d1d4c58
ravenblack24 <mysweetr0se@hotmail.com>
2020-04-08 15:34:51 +0100

    Create FCC-Algorithms-FindLongestWordInAString.js

---
+++ FCC-Algorithms-FindLongestWordInAString.js
@@ -0,0 +1,36 @@
+/**
+
+Basic Algorithm Scripting: Find the Longest Word in a String
+Return the length of the longest word in the provided sentence.
+
+Your response should be a number.
+
+Test cases
+
+findLongestWordLength("The quick brown fox jumped over the lazy dog") should return a number.
+Passed
+findLongestWordLength("The quick brown fox jumped over the lazy dog") should return 6.
+Passed
+findLongestWordLength("May the force be with you") should return 5.
+Passed
+findLongestWordLength("Google do a barrel roll") should return 6.
+Passed
+findLongestWordLength("What is the average airspeed velocity of an unladen swallow") should return 8.
+Passed
+findLongestWordLength("What if we try a super-long word such as otorhinolaryngology") should return 19.
+
+*/
+
+function findLongestWordLength(str) {
+let words = str.split(" ");
+var maxCount = 0;
+
+for(let i=0;i<words.length; i++){
+  if(words[i].length > maxCount){
+    maxCount = words[i].length;
+  }
+}
+  return maxCount;
+}
+
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
