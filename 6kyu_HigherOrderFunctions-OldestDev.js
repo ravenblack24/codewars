0f05c26f6444b5b624480ea2e86b023d71854530
ravenblack24 <mysweetr0se@hotmail.com>
2020-04-22 17:30:17 +0100

    Create 6kyu_HigherOrderFunctions-OldestDev.js

---
+++ 6kyu_HigherOrderFunctions-OldestDev.js
@@ -0,0 +1,86 @@
+/** 
+You will be given an array of objects representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+Your task is to return an array which includes the developer who is the oldest. In case of a tie, include all same-age senior developers listed in the same order as they appeared in the original input array.
+
+For example, given the following input array:
+
+var list1 = [
+  { firstName: 'Gabriel', lastName: 'X.', country: 'Monaco', continent: 'Europe', age: 49, language: 'PHP' },
+  { firstName: 'Odval', lastName: 'F.', country: 'Mongolia', continent: 'Asia', age: 38, language: 'Python' },
+  { firstName: 'Emilija', lastName: 'S.', country: 'Lithuania', continent: 'Europe', age: 19, language: 'Python' },
+  { firstName: 'Sou', lastName: 'B.', country: 'Japan', continent: 'Asia', age: 49, language: 'PHP' },
+];
+your function should return the following array:
+
+[
+  { firstName: 'Gabriel', lastName: 'X.', country: 'Monaco', continent: 'Europe', age: 49, language: 'PHP' },
+  { firstName: 'Sou', lastName: 'B.', country: 'Japan', continent: 'Asia', age: 49, language: 'PHP' },
+]
+Notes:
+
+The input array will always be valid and formatted as in the example above.
+
+est Cases:
+var list1 = [
+  { firstName: 'Mark', lastName: 'G.', country: 'Scotland', continent: 'Europe', age: 22, language: 'JavaScript' },
+  { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 30, language: 'Clojure' },
+  { firstName: 'Emma', lastName: 'B.', country: 'Norway', continent: 'Europe', age: 19, language: 'Clojure' }
+];
+
+var list2 = [
+  { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 29, language: 'Ruby' },
+  { firstName: 'Amar', lastName: 'V.', country: 'Bosnia and Herzegovina', continent: 'Europe', age: 32, language: 'Ruby' },
+];
+
+var list3 = [
+  { firstName: 'Sofia', lastName: 'P.', country: 'Italy', continent: 'Europe', age: 41, language: 'Clojure' },
+  { firstName: 'Jayden', lastName: 'P.', country: 'Jamaica', continent: 'Americas', age: 42, language: 'JavaScript' },
+  { firstName: 'Sou', lastName: 'B.', country: 'Japan', continent: 'Asia', age: 43, language: 'Python' },
+  { firstName: 'Rimas', lastName: 'C.', country: 'Jordan', continent: 'Asia', age: 44, language: 'Java' }
+];
+
+var list4 = [
+  { firstName: 'Alexander', lastName: 'F.', country: 'Russia', continent: 'Europe', age: 89, language: 'Java' },
+  { firstName: 'Fatima', lastName: 'K.', country: 'Saudi Arabia', continent: 'Asia', age: 21, language: 'Clojure' },
+  { firstName: 'Mark', lastName: 'G.', country: 'Scotland', continent: 'Europe', age: 22, language: 'JavaScript' },
+  { firstName: 'Nikola', lastName: 'H.', country: 'Serbia', continent: 'Europe', age: 29, language: 'Python' },
+  { firstName: 'Jakub', lastName: 'I.', country: 'Slovakia', continent: 'Europe', age: 28, language: 'Java' },
+  { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 89, language: 'JavaScript' },
+  { firstName: 'Luka', lastName: 'J.', country: 'Slovenia', continent: 'Europe', age: 29, language: 'Clojure' },
+  { firstName: 'Mariam', lastName: 'B.', country: 'Egypt', continent: 'Africa', age: 89, language: 'Python' },
+];
+
+var list5 = [
+  { firstName: 'Mehdi', lastName: 'H.', country: 'Tunisia', continent: 'Africa', age: 42, language: 'Python' },
+  { firstName: 'Yusuf', lastName: 'N.', country: 'Turkey', continent: 'Europe', age: 22, language: 'Python' },
+];
+
+console.log('List of developers: ', JSON.stringify(list1));
+Test.assertDeepEquals(findSenior(list1), findSeniorSolution(list1));
+
+console.log('List of developers: ', JSON.stringify(list2));
+Test.assertDeepEquals(findSenior(list2), findSeniorSolution(list2));
+
+console.log('List of developers: ', JSON.stringify(list3));
+Test.assertDeepEquals(findSenior(list3), findSeniorSolution(list3));
+
+console.log('List of developers: ', JSON.stringify(list4));
+Test.assertDeepEquals(findSenior(list4), findSeniorSolution(list4));
+
+console.log('List of developers: ', JSON.stringify(list5));
+Test.assertDeepEquals(findSenior(list5), findSeniorSolution(list5));
+*/
+
+function findSenior(list) {
+  
+  let oldestAge = 0;
+  
+  list.forEach((element) => {
+    if (element.age > oldestAge) {
+        oldestAge = element.age;
+      }
+  });
+  
+  return list.filter((oldestDev => oldestDev.age == oldestAge));
+}
