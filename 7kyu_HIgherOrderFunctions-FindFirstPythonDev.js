e352c93ad569f3cf3a73a0d49cbff871c519f0f8
ravenblack24 <mysweetr0se@hotmail.com>
2020-04-22 15:10:12 +0100

    Create 7kyu_HIgherOrderFunctions-FindFirstPythonDev.js

---
+++ 7kyu_HIgherOrderFunctions-FindFirstPythonDev.js
@@ -0,0 +1,54 @@
+/**
+You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising. The list is ordered according to who signed up first.
+
+Your task is to return one of the following strings:
+
+< firstName here >, < country here > of the first Python developer who has signed up; or
+There will be no Python developers if no Python developer has signed up.
+For example, given the following input array:
+
+var list1 = [
+  { firstName: 'Mark', lastName: 'G.', country: 'Scotland', continent: 'Europe', age: 22, language: 'JavaScript' },
+  { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 30, language: 'Python' },
+  { firstName: 'Emma', lastName: 'B.', country: 'Norway', continent: 'Europe', age: 19, language: 'Clojure' }
+];
+your function should return Victoria, Puerto Rico.
+
+Notes:
+
+The input array will always be valid and formatted as in the example above.
+
+Test Cases:
+var list1 = [
+  { firstName: 'Mark', lastName: 'G.', country: 'Scotland', continent: 'Europe', age: 22, language: 'JavaScript' },
+  { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 30, language: 'Python' },
+  { firstName: 'Emma', lastName: 'B.', country: 'Norway', continent: 'Europe', age: 19, language: 'Clojure' }
+];
+
+var list2 = [
+  { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 29, language: 'JavaScript' },
+  { firstName: 'Amar', lastName: 'V.', country: 'Bosnia and Herzegovina', continent: 'Europe', age: 32, language: 'Ruby' },
+];
+
+var list3 = [
+  { firstName: 'Sofia', lastName: 'P.', country: 'Italy', continent: 'Europe', age: 41, language: 'Clojure' },
+  { firstName: 'Jayden', lastName: 'P.', country: 'Jamaica', continent: 'Americas', age: 42, language: 'JavaScript' },
+  { firstName: 'Sou', lastName: 'B.', country: 'Japan', continent: 'Asia', age: 43, language: 'Python' },
+  { firstName: 'Rimas', lastName: 'C.', country: 'Jordan', continent: 'Asia', age: 44, language: 'Java' }
+];
+
+console.log('List of developers: ', JSON.stringify(list1));
+Test.assertEquals(getFirstPython(list1), getFirstPythonSolution(list1));
+
+console.log('List of developers: ', JSON.stringify(list2));
+Test.assertEquals(getFirstPython(list2), getFirstPythonSolution(list2));
+
+console.log('List of developers: ', JSON.stringify(list3));
+Test.assertEquals(getFirstPython(list3), getFirstPythonSolution(list3));
+
+*/
+
+function getFirstPython(list) {
+  const index = list.findIndex((attendee) =>  attendee.language === "Python");
+  return (index > -1) ? `${list[index].firstName}, ${list[index].country}` : 'There will be no Python developers';
+}
