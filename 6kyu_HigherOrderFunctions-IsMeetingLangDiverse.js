3e722f88b7a7e6482fd1fc7bb840f81fe8f878a4
ravenblack24 <mysweetr0se@hotmail.com>
2020-04-23 18:18:04 +0100

    Create 6kyu_HigherOrderFunctions-IsMeetingLangDiverse.js

---
+++ 6kyu_HigherOrderFunctions-IsMeetingLangDiverse.js
@@ -0,0 +1,81 @@
+/**
+You will be given an array of objects representing data about developers who have signed up to attend the next web development meetup that you are organising. Three programming languages will be represented: Python, Ruby and JavaScript.
+
+Your task is to return either:
+
+true if the number of meetup participants representing any of the three programming languages is ** at most 2 times higher than the number of developers representing any of the remaining programming languages**; or
+false otherwise.
+For example, given the following input array:
+
+var list1 = [
+  { firstName: 'Daniel', lastName: 'J.', country: 'Aruba', continent: 'Americas', age: 42, language: 'Python' },
+  { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 22, language: 'Ruby' },
+  { firstName: 'Sou', lastName: 'B.', country: 'Japan', continent: 'Asia', age: 43, language: 'Ruby' },
+  { firstName: 'Hanna', lastName: 'L.', country: 'Hungary', continent: 'Europe', age: 95, language: 'JavaScript' },
+  { firstName: 'Jayden', lastName: 'P.', country: 'Jamaica', continent: 'Americas', age: 18, language: 'JavaScript' },
+  { firstName: 'Joao', lastName: 'D.', country: 'Portugal', continent: 'Europe', age: 25, language: 'JavaScript' }
+];
+your function should return false as the number of JavaScript developers (3) is 3 times higher than the number of Python developers (1). It can't be more than 2 times higher to be regarded as language-diverse.
+
+Notes:
+
+The strings representing all three programming languages will always be formatted in the same way (e.g. 'JavaScript' will always be formatted with upper-case 'J' and 'S'.
+The input array will always be valid and formatted as in the example above.
+Each of the 3 programming languages will always be represented.
+
+Test Cases:
+var list1 = [
+  { firstName: 'Daniel', lastName: 'J.', country: 'Aruba', continent: 'Americas', age: 42, language: 'Python' },
+  { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 22, language: 'Ruby' },
+  { firstName: 'Sou', lastName: 'B.', country: 'Japan', continent: 'Asia', age: 43, language: 'Ruby' },
+  { firstName: 'Hanna', lastName: 'L.', country: 'Hungary', continent: 'Europe', age: 95, language: 'JavaScript' },
+  { firstName: 'Jayden', lastName: 'P.', country: 'Jamaica', continent: 'Americas', age: 18, language: 'JavaScript' },
+  { firstName: 'Joao', lastName: 'D.', country: 'Portugal', continent: 'Europe', age: 25, language: 'JavaScript' }
+];
+
+var list2 = [
+  { firstName: 'Daniel', lastName: 'J.', country: 'Aruba', continent: 'Americas', age: 42, language: 'Python' },
+  { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 22, language: 'Ruby' },
+  { firstName: 'Hanna', lastName: 'L.', country: 'Hungary', continent: 'Europe', age: 95, language: 'JavaScript' },
+  { firstName: 'Jayden', lastName: 'P.', country: 'Jamaica', continent: 'Americas', age: 18, language: 'JavaScript' },
+  { firstName: 'Joao', lastName: 'D.', country: 'Portugal', continent: 'Europe', age: 25, language: 'JavaScript' }
+];
+
+var list3 = [
+  { firstName: 'Daniel', lastName: 'J.', country: 'Aruba', continent: 'Americas', age: 42, language: 'Python' },
+  { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 22, language: 'Ruby' },
+  { firstName: 'Joao', lastName: 'D.', country: 'Portugal', continent: 'Europe', age: 25, language: 'JavaScript' }
+];
+
+
+console.log('List of developers: ', JSON.stringify(list1));
+Test.assertEquals(isLanguageDiverse(list1), isLanguageDiverseSolution(list1));
+
+console.log('List of developers: ', JSON.stringify(list2));
+Test.assertEquals(isLanguageDiverse(list2), isLanguageDiverseSolution(list2));
+
+console.log('List of developers: ', JSON.stringify(list3));
+Test.assertEquals(isLanguageDiverse(list3), isLanguageDiverseSolution(list3));
+
+*/
+
+function isLanguageDiverse(list) {
+  
+  const langs = list.map((attendee) => attendee.language);
+  const representedLangs = ["Python", "Ruby", "JavaScript"];
+  
+  // Store count of languages in array
+  const langCount = representedLangs.reduce((sum, repLang) => { 
+  
+  let filterArray = langs.filter(devLang => devLang == repLang);
+  return sum.concat(filterArray.length);
+      
+  },[])
+  
+  // Determine half of largest count value
+  const halfMax = (Math.max(...langCount))/2;
+  
+  // Check language counts are all at least half the largest count
+  return langCount.every((item) => { return item >= halfMax })
+  
+}
