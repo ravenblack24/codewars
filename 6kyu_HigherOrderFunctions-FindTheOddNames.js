3e92fc597091283ddb3c7646b29fb18c8aea73ab
ravenblack24 <mysweetr0se@hotmail.com>
2020-04-24 13:17:34 +0100

    Create 6kyu_HigherOrderFunctions-FindTheOddNames.js

---
+++ 6kyu_HigherOrderFunctions-FindTheOddNames.js
@@ -0,0 +1,75 @@
+/**
+You will be given an array of objects representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+Given the following input array:
+
+var list1 = [
+  { firstName: 'Aba', lastName: 'N.', country: 'Ghana', continent: 'Africa', age: 21, language: 'Python' },
+  { firstName: 'Abb', lastName: 'O.', country: 'Israel', continent: 'Asia', age: 39, language: 'Java' }
+];
+write a function that when executed as findOddNames(list1) returns only the developers where if you add the ASCII representation of all characters in their first names, the result will be an odd number:
+
+[
+  { firstName: 'Abb', lastName: 'O.', country: 'Israel', continent: 'Asia', age: 39, language: 'Java' }
+]
+Explanation of the above:
+
+Sum of ASCII codes of letters in 'Aba' is: 65 + 98 + 97 = 260 which is an even number
+Sum of ASCII codes of letters in 'Abb' is: 65 + 98 + 98 = 261 which is an odd number
+Notes:
+
+Preserve the order of the original list.
+Return an empty array [] if there is no developer with an "odd" name.
+The input array and first names will always be valid and formatted as in the example above.
+
+Test Cases:
+var list1 = [
+  { firstName: 'Harry', lastName: 'K.', country: 'Brazil', continent: 'Americas', age: 19, language: 'Python' }
+];
+
+var list2 = [
+  { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 29, language: 'Ruby' },
+  { firstName: 'Amar', lastName: 'V.', country: 'Bosnia and Herzegovina', continent: 'Europe', age: 32, language: 'Ruby' },
+];
+
+var list3 = [
+  { firstName: 'Sofia', lastName: 'P.', country: 'Italy', continent: 'Europe', age: 41, language: 'Clojure' },
+  { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 29, language: 'JavaScript' },
+  { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 39, language: 'Ruby' },
+  { firstName: 'Noa', lastName: 'A.', country: 'Israel', continent: 'Asia', age: 40, language: 'Ruby' },
+  { firstName: 'Andrei', lastName: 'E.', country: 'Romania', continent: 'Europe', age: 59, language: 'C' },
+  { firstName: 'Maria', lastName: 'S.', country: 'Peru', continent: 'Americas', age: 60, language: 'C' },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 75, language: 'Python' },
+  { firstName: 'Chloe', lastName: 'K.', country: 'Guernsey', continent: 'Europe', age: 88, language: 'Ruby' },
+  { firstName: 'Viktoria', lastName: 'W.', country: 'Bulgaria', continent: 'Europe', age: 98, language: 'PHP' },
+  { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'JavaScript' }
+];
+
+console.log('List of developers: ', JSON.stringify(list1));
+Test.assertDeepEquals(findOddNames(list1), findOddNamesSolution(list1));
+
+console.log('List of developers: ', JSON.stringify(list2));
+Test.assertDeepEquals(findOddNames(list2), findOddNamesSolution(list2));
+
+console.log('List of developers: ', JSON.stringify(list3));
+Test.assertDeepEquals(findOddNames(list3), findOddNamesSolution(list3));
+*/
+
+function findOddNames(list) {
+
+const myList = [...list];
+
+  return myList.reduce((oddResults, attendee) => {
+    
+    const letterArray = attendee.firstName.split("");
+    
+    const asciiCount = letterArray.reduce((count, letter) => {
+      count += letter.charCodeAt();
+      return count;
+    }
+    , 0);
+    
+    return (asciiCount % 2 != 0) ? oddResults.concat(attendee) : oddResults;
+
+  }, []);
+}
