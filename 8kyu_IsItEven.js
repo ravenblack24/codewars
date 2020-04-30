94727bcbf57578c1c71596c794ad0d1a1493c31e
ravenblack24 <mysweetr0se@hotmail.com>
2020-04-30 14:29:09 +0100

    Create 8kyu_IsItEven.js

---
+++ 8kyu_IsItEven.js
@@ -0,0 +1,27 @@
+/**
+In this Kata we are passing a number (n) into a function.
+
+Your code will determine if the number passed is even (or not).
+
+The function needs to return either a true or false.
+
+Numbers may be positive or negative, integers or floats.
+
+Floats are considered UNeven for this kata.
+
+Test Cases:
+Test.assertEquals(testEven(0), true, "testEven for 0");
+Test.assertEquals(testEven(0.5), false, "testEven for 0.5");
+Test.assertEquals(testEven(1), false, "testEven for 1");
+Test.assertEquals(testEven(2), true, "testEven for 2");
+Test.assertEquals(testEven(-4), true, "testEven for 2");
+Test.assertEquals(testEven(15), false, "testEven for 15");
+Test.assertEquals(testEven(20), true, "testEven for 20");
+Test.assertEquals(testEven(220), true, "testEven for 220");
+Test.assertEquals(testEven(222222221), false, "testEven for 222222221");
+Test.assertEquals(testEven(500000000000), true, "testEven for 500000000000");
+*/
+
+function testEven(n) {
+    return n % 2 == 0;
+}
